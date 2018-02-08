f7e903d3b2b1bfc6c3f00c8e924086df7cfc89b0
zhangyp <yunpeng.zhang@yuemia.com>
2018-02-08 19:06:57 +0800

    css微调

--- static/main/js/searchpop.js
+++ static/main/js/searchpop.js
@@ -1,20 +1,22 @@
 $(function() {
 
 			$('.search_Btn').click(function() {
-
-
+				layui.use(['layer'],function () {
+					var layer=layui.layer;
 					layer.open({
-						type: 2,
-						title: false,
-						closeBtn: 1, //不显示关闭按钮
-						shade: [0.4],
-						area: ['700px', '580px'],
-						offset: '40px',
-						scrollbar: false, //禁止浏览器滚动
-						anim: 2,
-						content: ['/api/apoint/search/?keyword='+$("#serch").val(), 'yes'],
-						end: function() {}
-					});
+								type: 2,
+								title: false,
+								closeBtn: 1, //不显示关闭按钮
+								shade: [0.4],
+								area: ['700px', '580px'],
+								offset: '40px',
+								scrollbar: false, //禁止浏览器滚动
+								anim: 2,
+								content: ['/api/apoint/search/?keyword='+$("#serch").val(), 'yes'],
+								end: function() {}
+							});
+    })
+
 
 		});
 })
