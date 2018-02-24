9dbebcc7cc2c304d4ef0ed4e0d9783d7a6e53016
yangcheng <yangcheng.fan@yuemia.com>
2018-02-24 15:43:15 +0800

    修改xadmin

--- apoint/adminx.py
+++ apoint/adminx.py
@@ -2,9 +2,12 @@
 import xadmin
 from models import *
 #class ZJUseradmin(object):
+class OrderDetailDisplay(object):
+    search_fields=['creater',]
+
 xadmin.site.register(ZJUser)
 xadmin.site.register(SalesUser)
 xadmin.site.register(Order) #患者订单
 xadmin.site.register(Area)#省
 xadmin.site.register(Hospital)#医院
-xadmin.site.register(OrderDetail)#客服描述
+xadmin.site.register(OrderDetail,OrderDetailDisplay)#客服描述
