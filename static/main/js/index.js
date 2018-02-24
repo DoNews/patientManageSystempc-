a90fa4547aec550244561fd8bcef4fc055708828
muzishan <suhua.liu@yuemia.com>
2018-02-24 18:38:27 +0800

    btn default

--- static/main/js/index.js
+++ static/main/js/index.js
@@ -25,7 +25,6 @@ $(function () {
         })
         $('.jrrw_list_btn').click(function () {
             var sid = $(this).attr("id");
-
             layer.open({
                 type: 2,
                 title: false,
@@ -43,12 +42,29 @@ $(function () {
 
 
         })
-
         function reloadToday() {
             $.ajax("/todaywork", {
                 dataType: "json",
                 success: function (data) {
                     $(".jrrw_list").html(data.data)
+                            $('.jrrw_list_btn').click(function () {
+                                var sid = $(this).attr("id");
+                                layer.open({
+                                    type: 2,
+                                    title: false,
+                                    closeBtn: 1, //不显示关闭按钮
+                                    shade: [0.4],
+                                    area: ['800px', '550px'],
+                                    offset: '40px',
+                                    scrollbar: false, //禁止浏览器滚动
+                                    anim: 2,
+                                    content: ['/orderdetail?id=' + sid, 'yes'], //iframe的url，no代表不显示滚动条
+                                    end: function () { //此处用于演示
+                                        reloadToday()
+                                    }
+                                });
+                            })
+
                 }
 
             })
